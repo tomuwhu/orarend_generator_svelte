583d4173f7b6abe04085a9c53e2cd6b6f373fe76
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-23 13:02:08 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/orarend_from_server',
+  base: '/orarend_generator_svelte',
   build: {
     outDir: 'docs',
     emptyOutDir: true
